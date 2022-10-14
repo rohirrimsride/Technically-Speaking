520dfa639df2f3f2b016c3b4be0aab334e1473f0
Strider <dave.barnes23@gmail.com>
2022-10-13 21:14:33 -0500

    commented out single edit for now

--- controller/apis/index.js
+++ controller/apis/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const commentRoutes = require('./comment-routes');
 const postRoutes = require('./post-routes');
 const userRoutes = require('./user-routes');
-const singlePost = require('./single-post.js');
+// const singlePost = require('./single-post.js');
 
 router.use('/comments', commentRoutes);
 router.use('/posts', postRoutes);
