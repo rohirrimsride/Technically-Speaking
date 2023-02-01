5424303a5e07d372cc4b87d23a834a387bb4c2a7
Strider <dave.barnes23@gmail.com>
2023-02-01 17:46:48 -0600

    removed the secret from the server.js file and added it to the .env filele

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: 'Super-Duper-Puper-Scuper',
+    secret: process.env.DB_SECRET,
     cookie: { },
     resave: false,
     saveUninitialized: true,
