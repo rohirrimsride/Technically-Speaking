aae42bf900a4b05f5e068c6c00e5e53c276f443d
Strider <dave.barnes23@gmail.com>
2023-02-14 16:36:39 -0600

    committing before restarting PC

--- config/connection.js
+++ config/connection.js
@@ -9,12 +9,15 @@ let sequelize;
 // if (process.env.JAWSDB_URL) {
 //     sequelize = new Sequelize(process.env.JAWSDB_URL);
 
+// Supabase database setup and access
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 if (supabase) {
     sequelize = new Sequelize(supabase);
+
+console.log('Supabase Instance: ', supabase);
 
 } else {
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
