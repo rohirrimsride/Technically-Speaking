4fa2b5f9f2b8d73f0dc98fe43509b975fe5c0ddb
Strider <dave.barnes23@gmail.com>
2023-02-19 18:17:33 -0600

    working on setting up the render deployment config

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,28 @@
 require('dotenv').config();
 
-const { createClient } = require('@supabase/supabase-js');
+// const { createClient } = require('@supabase/supabase-js');
 const Sequelize = require('sequelize');
 
 let sequelize;
 
-//This is for the Heroku setup commenting out until I figure out the Vercel/Supabase setup 
+// This is for the Heroku setup commenting out until I figure out the Vercel/Supabase setup 
 // if (process.env.JAWSDB_URL) {
 //     sequelize = new Sequelize(process.env.JAWSDB_URL);
 
+// This is for the RENDER POSTGRESSQL setup
+if (process.env.RENDER_URL) {
+    sequelize = new Sequelize(process.env.RENDER_URL);
+
+
 // Supabase database setup and access
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey)
+// const supabaseUrl = process.env.SUPABASE_URL;
+// const supabaseKey = process.env.SUPABASE_KEY;
+// const supabase = createClient(supabaseUrl, supabaseKey)
 
-if (supabase) {
-    sequelize = new Sequelize(supabase);
+// if (supabase) {
+//     sequelize = new Sequelize(supabase);
 
-console.log('Supabase Instance: ', supabase);
+// console.log('Supabase Instance: ', supabase);
 
 } else {
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
