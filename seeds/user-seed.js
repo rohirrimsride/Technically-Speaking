bccb08c0dbf536266d2b1abd671e367a0dbd653b
Strider <dave.barnes23@gmail.com>
2023-02-27 16:10:15 -0600

    saving changes so I can switch to the correct branch

--- seeds/user-seed.js
+++ seeds/user-seed.js
@@ -2,8 +2,8 @@ const { User } = require('../model');
 
 const userData = [
     {
-        username: 'johnny5isAlive',
-        email: 'johnny5isAlive@80smail.com',
+        username: 'techtonic',
+        email: 'techtonic@shiftingstates.com',
         password: '$2b$12$Jmn3i0ZvdH/mpYOtPcnxPu80/OLUXRc.PLSMMUC4tHN06eDiYcEQy'
     },
     {
@@ -12,45 +12,10 @@ const userData = [
         password: 'Password1234'
     },
     {
-        username: 'johnny7isAlive',
-        email: 'johnny7isAlive@80smail.com',
+        username: 'WurdyBurdy',
+        email: 'highflyer@overthemoon.com',
         password: '$2b$12$Jmn3i0ZvdH/mpYOtPcnxPu80/OLUXRc.PLSMMUC4tHN06eDiYcEQy'
-    },
-    {
-        username: 'johnny8isAlive',
-        email: 'johnny8isAlive@80smail.com',
-        password: '$2b$12$Jmn3i0ZvdH/mpYOtPcnxPu80/OLUXRc.PLSMMUC4tHN06eDiYcEQy'
-    },
-    {
-        username: 'johnny9isAlive',
-        email: 'johnny9isAlive@80smail.com',
-        password: '$2b$12$Jmn3i0ZvdH/mpYOtPcnxPu80/OLUXRc.PLSMMUC4tHN06eDiYcEQy'
-    },
-    {
-        username: 'johnny10isAlive',
-        email: 'johnny10isAlive@80smail.com',
-        password: '$2b$12$Jmn3i0ZvdH/mpYOtPcnxPu80/OLUXRc.PLSMMUC4tHN06eDiYcEQy'
-    },
-    {
-        username: 'johnn11isAlive',
-        email: 'johnny11isAlive@80smail.com',
-        password: '$2b$12$Jmn3i0ZvdH/mpYOtPcnxPu80/OLUXRc.PLSMMUC4tHN06eDiYcEQy'
-    },
-    {
-        username: 'johnny12isAlive',
-        email: 'johnny12isAlive@80smail.com',
-        password: '$2b$12$Jmn3i0ZvdH/mpYOtPcnxPu80/OLUXRc.PLSMMUC4tHN06eDiYcEQy'
-    },
-    {
-        username: 'johnny13isAlive',
-        email: 'johnny13isAlive@80smail.com',
-        password: '$2b$12$Jmn3i0ZvdH/mpYOtPcnxPu80/OLUXRc.PLSMMUC4tHN06eDiYcEQy'
-    },
-    {
-        username: 'johnny22isAlive',
-        email: 'johnny22isAlive@80smail.com',
-        password: '$2b$12$Jmn3i0ZvdH/mpYOtPcnxPu80/OLUXRc.PLSMMUC4tHN06eDiYcEQy'
-    },
+    }
 ];
 
 const seedUser = () => User.bulkCreate(userData);
